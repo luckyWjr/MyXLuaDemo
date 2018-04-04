efdda76da4e744f6a842f4d6a4b644d5b8b0a88e
wangjiangrong <903036544@qq.com>
2018-04-04 16:16:15 +0800

    添加XLua Tools 和热更demo

---
+++ Assets/XLua/MyExamples/Hotfix/Editor/HotfixConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using XLua;
+
+namespace MyExamples {
+
+	public static class HotfixConfig {
+
+        [Hotfix]
+        public static List<Type> hotfixList {
+            get {
+                string[] allowNamespaces = new string[] {
+                    "MyExamples",
+                };
+
+                return (from type in Assembly.Load("Assembly-CSharp").GetTypes()
+                        where allowNamespaces.Contains(type.Namespace)
+                        select type).ToList();
+            }
+        }
+
+    }
+}
---
+++ Assets/XLua/MyExamples/Hotfix/Hotfix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace MyExamples {
+
+	public class Hotfix : MonoBehaviour {
+
+		void Start () {
+            XLuaManager.instance.Start();
+            Show();
+        }
+
+        void Show() {
+            Debug.Log("Show!!!");
+        }
+	}
+}
