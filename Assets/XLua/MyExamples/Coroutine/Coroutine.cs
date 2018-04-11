4181329ad1a1e5a580527a3ab0a5af6898ad3345
wangjiangrong <903036544@qq.com>
2018-04-11 19:10:35 +0800

    自己的协程demo

---
+++ Assets/XLua/MyExamples/Coroutine/Coroutine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace MyExamples {
+
+	public class Coroutine : MonoBehaviour {
+
+        LuaEnv luaenv = null;
+
+        void Start() {
+            luaenv = new LuaEnv();
+            luaenv.DoString("require 'mycoroutine'");
+        }
+
+        void Update() {
+            if(luaenv != null) {
+                luaenv.Tick();
+            }
+        }
+    }
+}
